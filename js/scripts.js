23931ea96154bcdcb190ede23b08674fe2f2ee19
Benjamin Benkert <ben@peakperformancepc.com>
2017-07-16 15:40:04 -0400

    Add latitude and longitude to API and test output

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
 latitude = position.coords.latitude;
 longitude = position.coords.longitude;
+$('#weatherData').html("https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude+"");
     });
 } else {
     alert("Geolocation is not enabled on this device")
