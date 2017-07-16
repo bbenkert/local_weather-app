e0153bf0eaae29e024584d67340f7572ff6ff11e
Benjamin Benkert <ben@peakperformancepc.com>
2017-07-16 15:55:26 -0400

    Add getJSON method, pull from API, test output to console

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,12 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
 latitude = position.coords.latitude;
 longitude = position.coords.longitude;
-$('#weatherData').html("https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude+"");
+var api = "https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude+"";
+$.getJSON(api, function(wdat) {
+var weather = wdat.weather[0].main;
+console.log(weather);
+
+});
     });
 } else {
     alert("Geolocation is not enabled on this device")
